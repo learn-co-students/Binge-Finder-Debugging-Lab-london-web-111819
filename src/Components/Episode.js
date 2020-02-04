c8d14bde59c6af8eafe613790cc08f66096551bb
badogan <basri.dogan.71@gmail.com>
2020-02-04 15:55:43 +0000

    Done.

--- src/Components/Episode.js
+++ src/Components/Episode.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Episode = (props) => {
-  let { myEpisode } = props
-
+  // let { eachEpisode } = props
+  // debugger
   return (
     <div>
-      Episode {myEpisode.number} - {myEpisode.name}
+      Episode {props.eachEpisode.number}  - {props.eachEpisode.name}
     </div>
   )
 }
